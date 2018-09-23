1acf17b9abe2de5b91c443201c7a534632c2f1eb
Roberto Samia <bojosteph@gmail.com>
2018-09-23 01:16:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 
-       sentence1 => ("Hello there, and how are you?") 
-sentence2 => ("Hi again, just making sure it's reversed!") 
+ hash = { sentence1 => "Hello there, and how are you?" 
+sentence2 => "Hi again, just making sure it's reversed!"}
+
+def reverse_each
 
 
 
