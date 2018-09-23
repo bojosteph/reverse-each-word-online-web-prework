c04b89fb819cdfe4430463bdb9418e278e2a97a3
Roberto Samia <bojosteph@gmail.com>
2018-09-23 01:40:39 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,12 @@ def reverse_each_word(array)
   word = [sentence1, sentence2]
   word.split = new_word
   new_word.each do |sentence|
-    
+      sentence.reverse!
+  return sentence
+end
+
+
+def 
   
   
 
